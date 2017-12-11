c083300335cd009c7767c4f07b847336ec979716
Dennis <dennisent@protonmail.com>
2017-12-11 16:40:23 +1300

    Add files via upload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 
@@ -17,6 +18,15 @@ class LoginForm extends Component {
     this.props.loginUser({ email, password });
   }
 
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+        </View>
+      );
+    }
+  }
   render() {
     return (
       <Card>
@@ -38,6 +48,7 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+        {this.renderError()}
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
         </CardSection>
@@ -45,10 +56,19 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
-    password: state.auth.password
+    password: state.auth.password,
+    error: state.auth.error
   };
 };
 export default connect(mapStateToProps, {
