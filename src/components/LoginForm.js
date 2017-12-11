b8fd8b4c1a2812560dbbce8909b44f593503f74d
Dennis <dennisent@protonmail.com>
2017-12-11 13:44:42 +1300

    Add files via upload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
-    return <Card />;
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@email.com"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input secureTextEntry label="Password" placeholder="password" />
+        </CardSection>
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+      </Card>
+    );
   }
 }
-
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(
+  LoginForm
+);
