374f04947aadc7c6c36b9a8047457de49ef7468e
Dennis <dennisent@protonmail.com>
2017-12-12 19:26:25 +1300

    Add files via upload

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,8 +1,24 @@
 import { EMPLOYEE_UPDATE } from './types';
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+  // no action required. So using redux thunk hack
+  return () => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employess`)
+      .push({ name, phone, shift })
+      .then(() => {
+        Actions.main({ type: 'reset' });
+      });
   };
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
   render() {
     return (
       <Card>
@@ -46,7 +50,7 @@ class EmployeeCreate extends Component {
           </Picker>
         </CardSection>
         <CardSection>
-          <Button>Create</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Create</Button>
         </CardSection>
       </Card>
     );
@@ -64,4 +68,6 @@ const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
   return { name, phone, shift };
 };
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(
+  EmployeeCreate
+);
