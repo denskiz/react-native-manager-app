7104a25118c09f098fba29d0bed2fb02efa436d5
Dennis <dennisent@protonmail.com>
2017-12-13 14:32:41 +1300

    Add files via upload

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,24 +1,33 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
   }
+
+  renderItem({ item }) {
+    return <ListItem employee={item} />;
+  }
   render() {
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <FlatList data={this.props.employees} renderItem={this.renderItem} />
     );
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  //Use lodash to convert an object into an array
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+    // {shift: 'Monday, name: 'S', id: 'idwaod'}
+  });
+  return { employees };
+};
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
+
+//dennis
